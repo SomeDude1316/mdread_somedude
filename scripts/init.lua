391fdd0c87703b2698c906e9fd026d0a2f7ce115
SomeDude1316 <alapinski955@gmail.com>
2022-06-14 00:52:26 -0500

    Implement maps and add to layout

--- scripts/init.lua
+++ scripts/init.lua
@@ -3,6 +3,7 @@ ScriptHost:LoadScript("scripts/settings.lua")
 ScriptHost:LoadScript("scripts/logic.lua")
 
 Tracker:AddItems("items/items.json")
+Tracker:AddMaps("maps/maps.json")
 
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
