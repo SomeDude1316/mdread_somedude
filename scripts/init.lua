2d611d1da6656af120d65d09ce2845e867ba47e6
SomeDude1316 <alapinski955@gmail.com>
2022-06-15 16:47:06 -0500

    Update init.lua

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,11 +1,24 @@
--- Load items and layout
+-- Load logic
 ScriptHost:LoadScript("scripts/settings.lua")
 ScriptHost:LoadScript("scripts/itemlogic.lua")
 ScriptHost:LoadScript("scripts/arealogic.lua")
 
+-- Load items
 Tracker:AddItems("items/items.json")
-Tracker:AddMaps("maps/maps.json")
-Tracker:AddLocations("locations/locations.json")
 
+-- Load area maps
+Tracker:AddMaps("maps/maps.json")
+
+-- Load locations by area
+Tracker:AddLocations("locations/artaria.json")
+Tracker:AddLocations("locations/burenia.json")
+Tracker:AddLocations("locations/cataris.json")
+Tracker:AddLocations("locations/dairon.json")
+Tracker:AddLocations("locations/elun.json")
+Tracker:AddLocations("locations/ferenia.json")
+Tracker:AddLocations("locations/ghavoran.json")
+Tracker:AddLocations("locations/hanubia.json")
+
+-- Load tracker layouts
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
