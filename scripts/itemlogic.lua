dcfaf5c2ae78df268f1e8902d69a57547c9d763d
SomeDude1316 <alapinski955@gmail.com>
2022-06-15 00:41:36 -0500

    Refactor item logic, initialize area logic

---
+++ scripts/itemlogic.lua
@@ -0,0 +1,143 @@
+-- Item Collection
+
+function hasItem(item)
+    return Tracker:ProviderCountForCode(item) > 0 
+end
+
+function missing(item)
+    return not hasItem(item)
+end
+
+function countTanks()
+    return Tracker:ProviderCountForCode("etank") + (Tracker:ProviderCountForCode("epart") / 4)
+end
+
+-- Traversal Ability
+
+function canHellRun(num)
+    if hasItem("varia") or countTanks() >= num then
+        return 1
+    end
+    return 0
+end
+
+function canColdRun(num)
+    if hasItem("gravity") or countTanks() >= num then
+        return 1
+    end
+    return 0
+end
+
+function canBomb()
+    if hasItem("morph") and (hasItem("bomb") or hasItem("cross")) then
+        return 1
+    end
+    return 0
+end
+
+function canPowerBomb()
+    if hasItem("morph") and hasItem("pb") then
+        return 1
+    end
+    return 0
+end
+
+function canWBJ()
+    if hasItem("morph") and hasItem("bomb") and missing("gravity") then
+        return 1
+    end
+    return 0
+end
+
+function canWSJ()
+    if hasItem("space") and missing("gravity") then
+        return 1
+    end
+    return 0
+end
+
+function canFly()
+    if canBomb() > 0 or hasItem("space") then
+        return 1
+    end
+    return 0
+end
+
+function canClimb()
+    if hasItem("morph") or hasItem("flash") or canFly() > 0 then
+        return 1
+    end
+    return 0
+end
+
+function canOpenChargeDoors()
+    if hasItem("charge") or canPowerBomb() then
+        return 1
+    end
+    return 0
+end
+
+function canOpenGreenDoors()
+    if hasItem("missile") then
+        return 1
+    end
+    return 0
+end
+
+function canOpenWideDoors()
+    if hasItem("beam") then
+        return 1
+    end
+    return 0
+end
+
+function canMoveWideBlocks()
+    if canOpenWideDoors() and hasItem("charge") then
+        return 1
+    end
+    return 0
+end
+
+function canBreakDiffBlobs()
+    if hasItem("diffusion") or hasItem("wave") then
+        return 1
+    end
+    return 0
+end
+
+function canShortBoost()
+    if hasItem("flash") and hasItem("speed") then
+        return 1
+    end
+    return 0
+end
+
+function canSSC()
+    if canShortBoost() and hasItem("grapple") then
+        return 1
+    end
+    return 0
+end
+
+-- Event Functions
+
+function eventQuietRobe()
+    if hasItem("qr") then
+        return 1
+    end
+    return 0
+end
+
+function eventReleaseX()
+    if hasItem("x") then
+        return 1
+    end
+    return 0
+end
+
+function emmiDefeated(color)
+    if hasItem(color) then
+        return 1
+    end
+    return 0
+end
